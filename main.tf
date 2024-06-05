544b044adb37c8fb74c35501b31bc698a885ae3d
bcm754@naver.com <bcm754@naver.com>
2024-06-05 15:31:58 +0900

    k8s install

--- main.tf
+++ main.tf
@@ -27,7 +27,8 @@ resource "libvirt_domain" "ubuntu" {
   cloudinit  = libvirt_cloudinit_disk.cloud-init[count.index].id
 
   provisioner "remote-exec" {
-    inline = [
+  inline = [
+    # Docker 설치
     "sudo apt-get update",
     "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
     "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
@@ -35,8 +36,55 @@ resource "libvirt_domain" "ubuntu" {
     "sudo apt-get update",
     "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
     "sudo systemctl start docker",
-    "sudo systemctl enable docker"
-    ]
+    "sudo systemctl enable docker",
+
+    # containerd 적용
+    "sudo mkdir -p /etc/containerd",
+    "containerd config default | sudo tee /etc/containerd/config.toml > /dev/null",
+    "sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml",
+    "sudo systemctl restart containerd",
+
+    # Kubernetes 설치
+    "swapoff --all",
+    "sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl",
+    "sudo mkdir -p /etc/apt/keyrings",
+    "sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+    "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+    "sudo apt-get update",
+    "sudo apt-get install -y kubelet kubeadm kubectl",
+    "sudo apt-mark hold kubelet kubeadm kubectl",
+
+    # kubelet 시작 및 활성화
+    "sudo systemctl start kubelet",
+    "sudo systemctl enable kubelet",
+
+    # kubeadm 인증서 추가
+    "sudo sed -i '/disabled_plugins/s/^/#/' /etc/containerd/config.toml",
+    "sudo systemctl restart containerd",
+    "sudo kubeadm config images pull --cri-socket unix:///run/containerd/containerd.sock",
+
+    # kub-config.yaml 파일 생성
+    "echo '---' | sudo tee /home/ubuntu/kub-config.yaml",
+    "echo 'apiVersion: \"kubeadm.k8s.io/v1beta3\"' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'kind: InitConfiguration' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'nodeRegistration:' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo '  criSocket: \"unix:///var/run/containerd/containerd.sock\"' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo '---' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'apiVersion: kubelet.config.k8s.io/v1beta1' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'kind: KubeletConfiguration' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'failSwapOn: false' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'featureGates:' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo '  NodeSwap: true' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'memorySwap:' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo '  swapBehavior: LimitedSwap' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo '---' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'apiVersion: kubeadm.k8s.io/v1beta3' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'kind: ClusterConfiguration' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo 'networking:' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    "echo '  podSubnet: \"172.24.0.0/24\" # --pod-network-cidr' | sudo tee -a /home/ubuntu/kub-config.yaml",
+    
+
+  ]
 
     connection {
       type     = "ssh"
